d22efd9578e5995bad3b06a90c7a9d679ca3c92e
Kennedy Mutuku <mutukukennedy5@gmail.com>
2026-03-12 13:15:04 +0300

    chore: increase request body limit to 50MB for large PDFs

--- server.js
+++ server.js
@@ -29,8 +29,8 @@ app.use(cors({
     },
     credentials: true,
 }));
-app.use(express.json({ limit: '15mb' }));
-app.use(express.urlencoded({ limit: '15mb', extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // DB Health Check Middleware - Fail fast if DB is disconnected
 const dbCheck = (req, res, next) => {
